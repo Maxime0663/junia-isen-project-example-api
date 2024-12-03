5ea8653bdde66fb7aa7ef857b7d46f0a9b75518e
Clementale <90531194+Clementale@users.noreply.github.com>
2024-12-03 17:09:58 +0100

    Ressources

---
+++ ressources/database/initdatabase.sql
@@ -0,0 +1,23 @@
+-- Création de la base de données
+CREATE TABLE Customers (
+    id_customer SERIAL PRIMARY KEY,
+    firstname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Orders (
+    id_order SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL REFERENCES Customers(id_customer) ON DELETE CASCADE,
+    product_list TEXT NOT NULL
+);
+
+-- Insertion de données
+INSERT INTO Customers (firstname, lastname) VALUES ('Emma', 'Johnson');
+INSERT INTO Customers (firstname, lastname) VALUES ('Liam', 'Brown');
+INSERT INTO Customers (firstname, lastname) VALUES ('Olivia', 'Taylor');
+INSERT INTO Customers (firstname, lastname) VALUES ('Noah', 'Williams');
+
+INSERT INTO Orders (customer_id, product_list) VALUES (1, '1,2,3');
+INSERT INTO Orders (customer_id, product_list) VALUES (2, '4,5,6');
+INSERT INTO Orders (customer_id, product_list) VALUES (3, '7,8,9');
+INSERT INTO Orders (customer_id, product_list) VALUES (4, '10,1,2');
